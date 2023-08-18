882626d83be15345b5784145845a080bf522f069
Omar Khaled Ahmed <136479749+OmarKhaledKhalil@users.noreply.github.com>
2023-08-18 19:28:45 +0300

    Update GPIO_Private.h

--- 01-MCAL/GPIO/GPIO_Private.h
+++ 01-MCAL/GPIO/GPIO_Private.h
@@ -12,32 +12,32 @@
 #define GPIOC_BASE_ADDRESS     0x40011000
 
              /* Register Addresses for PortA */
-#define GPIOA_CRL	*(volatile (u32*)(GPIOA_BASE_ADDRESS+0x00))
-#define GPIOA_CRH	*(volatile (u32*)(GPIOA_BASE_ADDRESS+0x04))
-#define GPIOA_IDR	*(volatile (u32*)(GPIOA_BASE_ADDRESS+0x08))
-#define GPIOA_ODR	*(volatile (u32*)(GPIOA_BASE_ADDRESS+0x0C))
-#define GPIOA_BSR	*(volatile (u32*)(GPIOA_BASE_ADDRESS+0x10))
-#define GPIOA_BRR	*(volatile (u32*)(GPIOA_BASE_ADDRESS+0x14))
-#define GPIOA_LCK	*(volatile (u32*)(GPIOA_BASE_ADDRESS+0x18))
+#define GPIOA_CRL	  *(volatile (u32*)(GPIOA_BASE_ADDRESS+0x00))
+#define GPIOA_CRH	  *(volatile (u32*)(GPIOA_BASE_ADDRESS+0x04))
+#define GPIOA_IDR	  *(volatile (u32*)(GPIOA_BASE_ADDRESS+0x08))
+#define GPIOA_ODR	  *(volatile (u32*)(GPIOA_BASE_ADDRESS+0x0C))
+#define GPIOA_BSrR	*(volatile (u32*)(GPIOA_BASE_ADDRESS+0x10))
+#define GPIOA_BRR	  *(volatile (u32*)(GPIOA_BASE_ADDRESS+0x14))
+#define GPIOA_LCK	  *(volatile (u32*)(GPIOA_BASE_ADDRESS+0x18))
 
              /* Register Addresses for PortB */
-#define GPIOB_CRL	*(volatile (u32*)(GPIOB_BASE_ADDRESS+0x00))
-#define GPIOB_CRH	*(volatile (u32*)(GPIOB_BASE_ADDRESS+0x04))
-#define GPIOB_IDR	*(volatile (u32*)(GPIOB_BASE_ADDRESS+0x08))
-#define GPIOB_ODR	*(volatile (u32*)(GPIOB_BASE_ADDRESS+0x0C))
-#define GPIOB_BSR	*(volatile (u32*)(GPIOB_BASE_ADDRESS+0x10))
-#define GPIOB_BRR	*(volatile (u32*)(GPIOB_BASE_ADDRESS+0x14))
-#define GPIOB_LCK	*(volatile (u32*)(GPIOB_BASE_ADDRESS+0x18))
+#define GPIOB_CRL	  *(volatile (u32*)(GPIOB_BASE_ADDRESS+0x00))
+#define GPIOB_CRH	  *(volatile (u32*)(GPIOB_BASE_ADDRESS+0x04))
+#define GPIOB_IDR	  *(volatile (u32*)(GPIOB_BASE_ADDRESS+0x08))
+#define GPIOB_ODR	  *(volatile (u32*)(GPIOB_BASE_ADDRESS+0x0C))
+#define GPIOB_BSRR	*(volatile (u32*)(GPIOB_BASE_ADDRESS+0x10))
+#define GPIOB_BRR	  *(volatile (u32*)(GPIOB_BASE_ADDRESS+0x14))
+#define GPIOB_LCK	  *(volatile (u32*)(GPIOB_BASE_ADDRESS+0x18))
 
 
              /* Register Addresses for PortC */
-#define GPIOC_CRL	*(volatile (u32*)(GPIOC_BASE_ADDRESS+0x00))
-#define GPIOC_CRH	*(volatile (u32*)(GPIOC_BASE_ADDRESS+0x04))
-#define GPIOC_IDR	*(volatile (u32*)(GPIOC_BASE_ADDRESS+0x08))
-#define GPIOC_ODR	*(volatile (u32*)(GPIOC_BASE_ADDRESS+0x0C))
-#define GPIOC_BSR	*(volatile (u32*)(GPIOC_BASE_ADDRESS+0x10))
-#define GPIOC_BRR	*(volatile (u32*)(GPIOC_BASE_ADDRESS+0x14))
-#define GPIOC_LCK	*(volatile (u32*)(GPIOC_BASE_ADDRESS+0x18))
+#define GPIOC_CRL	  *(volatile (u32*)(GPIOC_BASE_ADDRESS+0x00))
+#define GPIOC_CRH	  *(volatile (u32*)(GPIOC_BASE_ADDRESS+0x04))
+#define GPIOC_IDR	  *(volatile (u32*)(GPIOC_BASE_ADDRESS+0x08))
+#define GPIOC_ODR	  *(volatile (u32*)(GPIOC_BASE_ADDRESS+0x0C))
+#define GPIOC_BSRR	*(volatile (u32*)(GPIOC_BASE_ADDRESS+0x10))
+#define GPIOC_BRR	  *(volatile (u32*)(GPIOC_BASE_ADDRESS+0x14))
+#define GPIOC_LCK	  *(volatile (u32*)(GPIOC_BASE_ADDRESS+0x18))
 
 
 #endif
